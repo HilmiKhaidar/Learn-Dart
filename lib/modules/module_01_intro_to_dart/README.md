844b17ef400543825ec49a70569bb9a90f98172a
HilmiKhaidar <Hilmikhaidarnabilla@gmail.com>
2025-10-31 15:29:25 +0700

    Modul 1 - Intro to Dart

---
+++ lib/modules/module_01_intro_to_dart/README.md
@@ -0,0 +1,9 @@
+# Modul 1 — Intro to Dart
+
+## Tujuan
+- Mengenal bahasa pemrograman Dart.
+- Menjalankan kode pertama di Dart.
+- Memahami struktur dasar program.
+
+## Latihan
+1. Jalankan perintah berikut di terminal:
---
+++ lib/modules/module_01_intro_to_dart/main.dart
@@ -0,0 +1,8 @@
+// Pembelajaran Modul 1  - Intro to Dart
+// Dibuat oleh: Hilmi Khaidar N
+
+void main() {
+  print("Hello World!");
+  print("Selamat datang di Dart, Hilmi!");
+  print("Belajar Flutter mulai sekarang");
+}
